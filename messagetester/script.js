b2521b0d571b7a4d1f90ff1aa51ebd107b8628fc
Roope Palomaki <roope.palomaki@me.com>
2017-02-11 20:43:00 +0200

    Added message tester

---
+++ messagetester/script.js
@@ -0,0 +1,39 @@
+var sendMessage(message) {
+  window.parent.postMessage(message, "*");
+};
+
+var sendScore = function() {
+  sendMessage({
+    'messageType': 'SCORE',
+    'score': 100.0
+  });
+};
+
+var sendSave = function() {
+  sendMessage({
+    messageType: "SAVE",
+    gameState: {
+      playerItems: [
+        "Sword",
+        "Wizard Hat"
+      ],
+      score: 506.0 // Float
+    }
+  });
+};
+
+var sendLoadRequest = function() {
+  sendMessage({
+    'messageType': 'LOAD_REQUEST'
+  });
+};
+
+var sendSetting = function() {
+  sendMessage({
+    messageType: "SETTING",
+    options: {
+      "width": 400,
+      "height": 300
+    }
+  });
+};
